5f5c903e82e67272c1b6ea17069ba329365eec64
Tarasankar Kundu <tarasankar.kundu@tesco.com>
2022-02-24 16:18:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const notificationBtn = document.getElementById("enable");
+  const checkNotificationPromise = () => {
+    try {
+      Notification.requestPermission().then();
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
+
+  const askNotificationPermission = () => {
+    // function to actually ask the permissions
+    function handlePermission(permission) {
+      // set the button to shown or hidden, depending on what the user answers
+      if (
+        Notification.permission === "denied" ||
+        Notification.permission === "default"
+      ) {
+        notificationBtn.style.display = "block";
+      } else {
+        notificationBtn.style.display = "none";
+      }
+    }
+
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      console.log("This browser does not support notifications.");
+    } else {
+      if (checkNotificationPromise()) {
+        Notification.requestPermission().then((permission) => {
+          handlePermission(permission);
+        });
+      } else {
+        Notification.requestPermission(function (permission) {
+          handlePermission(permission);
+        });
+      }
+    }
+  };
+
+  const sendNotification = () => {
+    console.log(Notification.permission);
+    new Notification("Test", { body: "Test Notification" });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button id="enable" onClick={askNotificationPermission}>
+        Enable notifications
+      </button>
+      <button id="notify" onClick={sendNotification}>
+        Send Notification
+      </button>
     </div>
   );
 }
